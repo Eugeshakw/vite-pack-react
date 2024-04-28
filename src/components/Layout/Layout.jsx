5572e21833dcc5152d6a41f5a2c41f941d70203e
Eugeshakw <eugeshakw@gmail.com>
2024-04-28 14:31:30 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,12 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import { Header } from "../Header/Header";
+
 
 export const Layout = () => {
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Header/>
+      
      
       <Outlet />
     </Suspense>
